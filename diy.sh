4d064303a2dd72241728e1969c718b044db8c6e5
876310253 <31689011+876310253@users.noreply.github.com>
2020-03-05 20:49:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/coolsnowwolf/packages package/lede
+git clone https://github.com/Lienol/openwrt-package package/Lienol
+
+
